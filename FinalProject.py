a3c7a37178d3d5ac1179bbb3ab68a09bfd78ba86
Stephen Wu <wujstephen@gmail.com>
2019-04-15 20:47:46 -0400

    Functionalizing some lines for clarity

--- FinalProject.py
+++ FinalProject.py
@@ -2,6 +2,11 @@ import numpy as np
 import os
 import collections
 
+# Directory where data is stored.
+DATA_DIR = "data"
+
+# Number of most frequent words to consider.
+NUM_WORDS = 500
 
 def predict_truth(review, pos_counts, neg_counts, word_to_ind):
     neg_prob = 0
@@ -13,52 +18,58 @@ def predict_truth(review, pos_counts, neg_counts, word_to_ind):
 
     return pos_prob > neg_prob
 
+def populate_vectors(comment_truth, word_list):
+    num_examples = 0
+    for subdir, dirs, files in os.walk(DATA_DIR):
+        for file in files:
+            path_file = os.path.join(subdir, file)
+            if ".txt" in path_file:
+                with open(path_file) as f:
+                    first_line = f.readline()
+                #print (first_line)
+                truthful = "truthful" in path_file
 
+                first_line = first_line.replace(".", " ")
+                first_line = first_line.replace(",", " ")
+                first_line = first_line.replace("(", " ")
+                first_line = first_line.replace(")", " ")
+                first_line = first_line.replace("?", "")
+                first_line = first_line.replace("!", "")
+                first_line = first_line.replace(";", "")
 
-rootdir = "data"
+                first_line = first_line.split()
 
-num_examples = 0
+                comment_truth.append((first_line, truthful))
+
+                num_examples += 1
+
+                for word in first_line:
+                    word_list.append(word.lower())
+    return num_examples
+
+def get_top_words(word_list):
+    top_words_filthy = collections.Counter(word_list).most_common(NUM_WORDS)
+    top_words = []
+    for tuple in top_words_filthy:
+        top_words.append(tuple[0])
+    return top_words
+
+def get_word_to_ind(top_words):
+    '''
+    Given list of words usage with repetitions, return sorted list of most-frequent to least-frequent words (ids).
+    '''
+    word_to_ind = {}
+    for ind, word in enumerate(top_words):
+        word_to_ind[word] = ind
+    return word_to_ind
+
 word_list = []
 comment_truth = []
-for subdir, dirs, files in os.walk(rootdir):
-    for file in files:
-        path_file = os.path.join(subdir, file)
-        if ".txt" in path_file:
-            with open(path_file) as f:
-                first_line = f.readline()
-            #print (first_line)
-            truthful = "truthful" in path_file
+num_examples = populate_vectors(comment_truth, word_list)
+top_words = get_top_words(word_list)
+word_to_ind = get_word_to_ind(top_words)
 
-            first_line = first_line.replace(".", " ")
-            first_line = first_line.replace(",", " ")
-            first_line = first_line.replace("(", " ")
-            first_line = first_line.replace(")", " ")
-            first_line = first_line.replace("?", "")
-            first_line = first_line.replace("!", "")
-            first_line = first_line.replace(";", "")
-
-            first_line = first_line.split()
-
-            comment_truth.append((first_line, truthful))
-
-            num_examples += 1
-            for word in first_line:
-                word_list.append(word.lower())
-
-word_num = 500
-top_words_filthy = collections.Counter(word_list).most_common(word_num)
-
-top_words = []
-for tuple in top_words_filthy:
-    top_words.append(tuple[0])
-
-
-word_to_ind = {}
-for ind, word in enumerate(top_words):
-    word_to_ind[word] = ind
-#print (word_to_ind)
-
-x_matrix = np.zeros((num_examples, word_num))
+x_matrix = np.zeros((num_examples, NUM_WORDS))
 y_matrix = np.zeros((num_examples, 1))
 
 for ind, review in enumerate(comment_truth):
@@ -68,8 +79,8 @@ for ind, review in enumerate(comment_truth):
         if review[1]:
             y_matrix[ind] = 1
 
-pos_counts = [0] * word_num
-neg_counts = [0] * word_num
+pos_counts = [0] * NUM_WORDS
+neg_counts = [0] * NUM_WORDS
 
 for review_truth in comment_truth:
     comment = review_truth[0]
@@ -82,15 +93,9 @@ for review_truth in comment_truth:
             else:
                 neg_counts[word_to_ind[word]] += 1
 
-print(pos_counts)
-print(neg_counts)
-print(sum(pos_counts))
-print(sum(neg_counts))
-
 correct = 0
 predictions = [0, 0]
 for review_truth in comment_truth:
-    print(review_truth[1])
     prediction = predict_truth(review_truth[0], pos_counts, neg_counts, word_to_ind)
     predictions[0 if prediction else 1] += 1
     if review_truth[1] == prediction:
