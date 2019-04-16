df40b3b0afe1fd1ac43b1409e3a6f0f474f5ba60
Stephen Wu <wujstephen@gmail.com>
2019-04-15 20:59:26 -0400

    more cleanup + class-ifying NaiveBayes

--- FinalProject.py
+++ FinalProject.py
@@ -8,18 +8,7 @@ DATA_DIR = "data"
 # Number of most frequent words to consider.
 NUM_WORDS = 500
 
-def predict_truth(review, pos_counts, neg_counts, word_to_ind):
-    neg_prob = 0
-    pos_prob = 0
-    for word in review:
-        if word in word_to_ind:
-            pos_prob += np.log(pos_counts[word_to_ind[word]]/float(800))
-            neg_prob += np.log(neg_counts[word_to_ind[word]]/float(800))
-
-    return pos_prob > neg_prob
-
 def populate_vectors(comment_truth, word_list):
-    num_examples = 0
     for subdir, dirs, files in os.walk(DATA_DIR):
         for file in files:
             path_file = os.path.join(subdir, file)
@@ -41,11 +30,8 @@ def populate_vectors(comment_truth, word_list):
 
                 comment_truth.append((first_line, truthful))
 
-                num_examples += 1
-
                 for word in first_line:
                     word_list.append(word.lower())
-    return num_examples
 
 def get_top_words(word_list):
     top_words_filthy = collections.Counter(word_list).most_common(NUM_WORDS)
@@ -67,43 +53,66 @@ word_list = []
 comment_truth = []
 num_examples = populate_vectors(comment_truth, word_list)
 top_words = get_top_words(word_list)
+# print(top_words)
 word_to_ind = get_word_to_ind(top_words)
 
-x_matrix = np.zeros((num_examples, NUM_WORDS))
-y_matrix = np.zeros((num_examples, 1))
+class NaiveBayes:
+    def __init__(self):
+        self.pos_counts = [0] * NUM_WORDS
+        self.neg_counts = [0] * NUM_WORDS
 
-for ind, review in enumerate(comment_truth):
-    for word in review[0]:
-        if word in word_to_ind:
-            x_matrix[ind][word_to_ind[word]] = 1
-        if review[1]:
-            y_matrix[ind] = 1
+    def train(self, comment_truth, word_to_ind):
+        num_examples = len(comment_truth)
 
-pos_counts = [0] * NUM_WORDS
-neg_counts = [0] * NUM_WORDS
+        x_matrix = np.zeros((num_examples, NUM_WORDS))
+        y_matrix = np.zeros((num_examples, 1))
 
-for review_truth in comment_truth:
-    comment = review_truth[0]
-    truthfulness = review_truth[1]
+        for ind, review in enumerate(comment_truth):
+            for word in review[0]:
+                if word in word_to_ind:
+                    x_matrix[ind][word_to_ind[word]] = 1
+                if review[1]:
+                    y_matrix[ind] = 1
 
-    for word in comment:
-        if word in word_to_ind:
-            if truthfulness:
-                pos_counts[word_to_ind[word]] += 1
-            else:
-                neg_counts[word_to_ind[word]] += 1
 
-correct = 0
-predictions = [0, 0]
-for review_truth in comment_truth:
-    prediction = predict_truth(review_truth[0], pos_counts, neg_counts, word_to_ind)
-    predictions[0 if prediction else 1] += 1
-    if review_truth[1] == prediction:
-        correct += 1
+        for review_truth in comment_truth:
+            comment = review_truth[0]
+            truthfulness = review_truth[1]
 
-print ("Predictions: ", predictions)
-print ("Correct: ", correct)
-print ("Naive Bayes Accuracy: ", correct / float(1600))
+            for word in comment:
+                if word in word_to_ind:
+                    if truthfulness:
+                        self.pos_counts[word_to_ind[word]] += 1
+                    else:
+                        self.neg_counts[word_to_ind[word]] += 1
+
+    def predict(self, review, word_to_ind):
+        neg_prob = 0
+        pos_prob = 0
+        for word in review:
+            if word in word_to_ind:
+                pos_prob += np.log(self.pos_counts[word_to_ind[word]]/float(800))
+                neg_prob += np.log(self.neg_counts[word_to_ind[word]]/float(800))
+
+        return pos_prob > neg_prob
+
+def evaluate(model):
+    correct = 0
+    predictions = [0, 0]
+    for review_truth in comment_truth:
+        prediction = model.predict(review_truth[0], word_to_ind)
+        predictions[0 if prediction else 1] += 1
+        if review_truth[1] == prediction:
+            correct += 1
+
+    print ("Predictions: ", predictions)
+    print ("Correct: ", correct)
+    print ("Accuracy: ", correct / float(1600))
+
+NaiveBayesModel = NaiveBayes()
+NaiveBayesModel.train(comment_truth, word_to_ind)
+evaluate(NaiveBayesModel)
+
 
 #big_oof = ["hello", "the", "hotel", "was", "very", "nice", "but", "smelly"]
 #print (predict_truth(big_oof, pos_counts, neg_counts, word_to_ind))
@@ -119,6 +128,3 @@ for review in comment_truth:
     print (review[0])
     print (review[1])
 '''
-
-#print (examples)
-#print(topNWords)
